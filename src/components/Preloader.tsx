ec7eceaf84801b9f910cf186ef5738ffce453e6b
Max Heinrich <s89865@bht-berlin.de>
2024-07-22 21:30:53 +0200

    added license, updated package.json

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -10,7 +10,7 @@ export const Preloader: React.FC<PreloaderProps> = ({ progress }) => {
     return (
         <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
             <div className="flex flex-col justify-center items-center w-screen gap-2 max-w-xl">
-                <SattrackIcon size={52} />
+                <SattrackIcon size={52} color={"#000"}/>
                 <span className="font-medium text-2xl mb-4">Connecting to uplink...</span>
                 <Progress value={progress} className="w-[60%]" />
             </div>
