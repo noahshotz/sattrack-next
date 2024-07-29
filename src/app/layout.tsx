5db17847d974c3d069a6cc3e98a9f775bcf95b31
Max Heinrich <s89865@bht-berlin.de>
2024-07-29 23:32:51 +0200

    fixed theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,9 +33,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
